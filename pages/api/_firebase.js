fd2c8e50d5a85bb34398fd53fac0fb5fbd5d6ae8
mikeatcarbonundone <61215321+mikeatcarbonundone@users.noreply.github.com>
2021-09-28 12:49:46 -0600

    changed firebase admin

--- pages/api/_firebase.js
+++ pages/api/_firebase.js
@@ -1,9 +1,13 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("/service-account.json");
+//var serviceAccount = require("/service-account.json");
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: firebaseAdmin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
