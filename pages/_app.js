78d8d3dc662a845b65502a91996308dfae91393c
mikeatcarbonundone <61215321+mikeatcarbonundone@users.noreply.github.com>
2021-09-30 15:31:34 -0600

    my useeffect broke everything

--- pages/_app.js
+++ pages/_app.js
@@ -7,23 +7,23 @@ import DefaultLayout from "../layouts/default";
 function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout || DefaultLayout;
 
-  useEffect(() => {
-    if (process.env.NODE_ENV === "production") {
-      const logEvent = (url) => {
-        firebase.analytics().setCurrentScreen(url);
-        firebase.analytics().logEvent("screen_view");
-      };
+  // useEffect(() => {
+  //   if (process.env.NODE_ENV === "production") {
+  //     const logEvent = (url) => {
+  //       firebase.analytics().setCurrentScreen(url);
+  //       firebase.analytics().logEvent("screen_view");
+  //     };
 
-      routers.events.on("routeChangeComplete", logEvent);
-      //For First Page
-      logEvent(window.location.pathname);
+  //     routers.events.on("routeChangeComplete", logEvent);
+  //     //For First Page
+  //     logEvent(window.location.pathname);
 
-      //Remvove Event Listener after un-mount
-      return () => {
-        routers.events.off("routeChangeComplete", logEvent);
-      };
-    }
-  }, []);
+  //     //Remvove Event Listener after un-mount
+  //     return () => {
+  //       routers.events.off("routeChangeComplete", logEvent);
+  //     };
+  //   }
+  // }, []);
   return (
     <ThemeProvider>
       <Layout>
