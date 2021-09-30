45f845e232354500fc650d9b70484a8209626333
mikeatcarbonundone <61215321+mikeatcarbonundone@users.noreply.github.com>
2021-09-30 15:34:21 -0600

    try this

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "../styles/globals.css";
 import firebase from "utils/firebase";
 import { ThemeProvider } from "utils/theme.js";
@@ -7,23 +7,7 @@ import DefaultLayout from "../layouts/default";
 function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout || DefaultLayout;
 
-  // useEffect(() => {
-  //   if (process.env.NODE_ENV === "production") {
-  //     const logEvent = (url) => {
-  //       firebase.analytics().setCurrentScreen(url);
-  //       firebase.analytics().logEvent("screen_view");
-  //     };
-
-  //     routers.events.on("routeChangeComplete", logEvent);
-  //     //For First Page
-  //     logEvent(window.location.pathname);
-
-  //     //Remvove Event Listener after un-mount
-  //     return () => {
-  //       routers.events.off("routeChangeComplete", logEvent);
-  //     };
-  //   }
-  // }, []);
+  firebase.analytics();
   return (
     <ThemeProvider>
       <Layout>
