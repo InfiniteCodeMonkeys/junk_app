e62ef84f2888b14d5e5418e356baf30c4938322c
mikeatcarbonundone <61215321+mikeatcarbonundone@users.noreply.github.com>
2021-09-30 11:56:02 -0600

    await the object

--- pages/api/stripe-webhook.js
+++ pages/api/stripe-webhook.js
@@ -33,14 +33,15 @@ export default async (req, res) => {
     console.log(`stripeEvent: ${stripeEvent.type}`);
 
     // Get the object from stripeEvent
-    const object = stripeEvent.data.object;
+    const object = await stripeEvent.data.object;
+    console.log(object.customer);
 
     switch (stripeEvent.type) {
       case "checkout.session.completed":
         //Get the order
 
-        const order = await getOrderByCustomerId(object.customer); //
-        // const order = await getOrderByCustomerId("cus_KIcs3cphRwoaUd");
+        // const order = await getOrderByCustomerId(object.customer); //
+        const order = await getOrderByCustomerId(object.customer);
 
         // Get the rate
         const rate = order.bestOption;
@@ -52,7 +53,7 @@ export default async (req, res) => {
           {
             rate: rate.object_id,
             label_file_type: "PDF",
-            async: true,
+            async: false,
           },
           async function (err, transaction) {
             // asynchronous callback
@@ -100,56 +101,6 @@ export default async (req, res) => {
         );
 
         break;
-
-      case "invoice.payment_succeeded":
-        // If a payment succeeded we update stored subscription status to "active"
-        // in case it was previously "trialing" or "past_due".
-        // We skip if amount due is 0 as that's the case at start of trial period.
-        if (object.amount_due > 0) {
-          await updateUserByCustomerId(object.customer, {
-            stripeSubscriptionStatus: "active",
-          });
-        }
-
-        break;
-
-      case "invoice.payment_failed":
-        // If a payment failed we update stored subscription status to "past_due"
-        await updateUserByCustomerId(object.customer, {
-          stripeSubscriptionStatus: "past_due",
-        });
-
-        break;
-
-      case "customer.subscription.updated":
-        await updateUserByCustomerId(object.customer, {
-          stripePriceId: object.items.data[0].price.id,
-          stripeSubscriptionStatus: object.status,
-        });
-
-        // 💡 You could also read "cancel_at_period_end" if you'd like to email user and learn why they cancelled
-        // or convince them to renew before their subscription is deleted at end of payment period.
-        break;
-
-      case "customer.subscription.deleted":
-        // If a subscription was deleted update stored subscription status to "canceled".
-        // Keep in mind this won't be called right away if "Cancel at end of billing period" is selected
-        // in Billing Portal settings (https://dashboard.stripe.com/settings/billing/portal). Instead you'll
-        // get a "customer.subscription.updated" event with a cancel_at_period_end value.
-        await updateUserByCustomerId(object.customer, {
-          stripeSubscriptionStatus: "canceled",
-        });
-
-        break;
-
-      case "customer.subscription.trial_will_end":
-        // This event happens 3 days before a trial ends
-        // 💡 You could email user letting them know their trial will end or you can have Stripe do that
-        // automatically 7 days in advance: https://dashboard.stripe.com/settings/billing/automatic
-
-        break;
-
-      // no default
     }
 
     // Send success response
