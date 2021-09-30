a9dfee25e0bafc4baf0eff063d451e6c27989f24
mikeatcarbonundone <61215321+mikeatcarbonundone@users.noreply.github.com>
2021-09-30 15:10:40 -0600

    really added analytics back in

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,29 @@
+import React, { useEffect } from "react";
 import "../styles/globals.css";
-import "utils/analytics.js";
+import firebase from "utils/firebase";
 import { ThemeProvider } from "utils/theme.js";
 import DefaultLayout from "../layouts/default";
 
 function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout || DefaultLayout;
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      const logEvent = (url) => {
+        firebase.analytics().setCurrentScreen(url);
+        firebase.analytics().logEvent("screen_view");
+      };
+
+      routers.events.on("routeChangeComplete", logEvent);
+      //For First Page
+      logEvent(window.location.pathname);
+
+      //Remvove Event Listener after un-mount
+      return () => {
+        routers.events.off("routeChangeComplete", logEvent);
+      };
+    }
+  }, []);
   return (
     <ThemeProvider>
       <Layout>
