6a0485dca57361300e180fe7ec0ecbe88c0b4264
mikeatcarbonundone <61215321+mikeatcarbonundone@users.noreply.github.com>
2021-10-19 12:37:03 -0600

    fixed async

--- pages/api/stripe-webhook.js
+++ pages/api/stripe-webhook.js
@@ -55,7 +55,7 @@ export default async (req, res) => {
             label_file_type: "PDF",
             async: false,
           })
-          .then(function async(err, transaction) {
+          .then(async (err, transaction) => {
             // asynchronous callback
 
             try {
